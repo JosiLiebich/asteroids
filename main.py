e3f187fabe429240aa227b4061bdb3b91f99b747
josephineliebich <josephine.liebich@uninow.de>
2025-09-24 17:35:23 +0200

    add groups for players

--- main.py
+++ main.py
@@ -8,21 +8,29 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     
+    dt = 0
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    Player.containers = (updatables, drawables)
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
    
-    dt = 0
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatables.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for p in drawables:
+            p.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
