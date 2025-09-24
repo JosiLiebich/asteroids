ac9b4fe15cf8d4a6420fec55bc235ae91037a36d
josephineliebich <josephine.liebich@uninow.de>
2025-09-24 19:21:47 +0200

    add split logic for asteroids

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
         for a in asteroids:
             for s in shots:
                 if a.collides_with(s):
-                    a.kill()
+                    a.split()
                     s.kill()
 
         for a in asteroids:
